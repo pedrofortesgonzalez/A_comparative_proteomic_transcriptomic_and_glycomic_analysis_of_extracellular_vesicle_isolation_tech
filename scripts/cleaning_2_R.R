45db281e940b3523b54710ef347551fd842db7d3
pedrofortesgonzalez <pedrofortesgonzalez94@gmail.com>
2025-05-19 13:19:38 +0200

    Update cleaning_2_R.R

--- scripts/cleaning_2_R.R
+++ scripts/cleaning_2_R.R
@@ -10,13 +10,13 @@ setwd(dirname(getActiveDocumentContext()$path))
 library(nolock)                                  # This library detects imported but unused packages
 
 
-# Detect unused packages in script n. 2
+# Detect unused packages in Script 2
 script_path <- "./2_hyptest_n_boxplots.R"        # Set script path
 libr_unused(script = script_path)                # Detect unused packages
 libr_used(script = script_path)                  # Detect used packages
 
 
-# Detect unused packages in script n. 3
+# Detect unused packages in Script 3
 script_path <- "./3_sector_diagrams.R"           # Set script path
 libr_unused(script = script_path)                # Detect unused packages
 libr_used(script = script_path)                  # Detect used packages
